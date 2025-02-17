cdfe75868e23135b17b6faabf3fe74340f6f3825
Sergio Lopes Junior <sergiolj@gmail.com>
2025-02-17 12:14:59 -0300

    Indentation adjust

--- src/javaRevisionExercises/RelationalOperators.java
+++ src/javaRevisionExercises/RelationalOperators.java
@@ -16,8 +16,8 @@ public class RelationalOperators extends Operand {
         String compare = getN1() == getN2()? "\nNumbers are identical" : "\nNumbers are different";
         System.out.println(compare);
         if(getN1()!=getN2()) {
-            String order = getN1() > getN2() ? "\nFirst number [" + getN1()
-                    + "] is greater than the second one [" + getN2() + "]" :
+            String order = getN1() > getN2() ?
+                    "\nFirst number [" + getN1() + "] is greater than the second one [" + getN2() + "]" :
                     "\nSecond number [" + getN2() + "] is greater than the first one [" + getN1() + "]";
             System.out.println(order);
         }
