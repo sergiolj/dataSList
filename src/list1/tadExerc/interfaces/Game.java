bf7b6a3758e72977a54e668dc56f9b23a13607ae
Sergio Lopes Junior <sergiolj@gmail.com>
2025-03-18 14:15:31 -0300

    Lista 2 Recursividade

--- src/list1/tadExerc/interfaces/Game.java
+++ src/list1/tadExerc/interfaces/Game.java
@@ -3,6 +3,9 @@ package list1.tadExerc.interfaces;
 public interface Game {
     void scoreSystem();
     void startGame();
+
+    void updateGame();
+
     void addGameResult();
     void displayResult();
     void electWinner();
